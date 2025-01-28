a7bf3cdcc6168c216bc049254e64faa05136cb5f
FranciscoMBicalho <franciscobicalho35@gmail.com>
2025-01-28 16:42:56 -0300

    Mudança no main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
+#include <limits>
 #include "include/cliente.hpp"
 #include "include/gerente.hpp"
 #include "include/pedido.hpp"
 #include "include/cardapio.hpp"
+#include "login.hpp"
 
 #define SUCESSO 0 
 #define LINE "\n----------------------------\n"
@@ -15,13 +17,48 @@ int main(){
 
     int cmd;
     std::string tmpNome, tmpEmail, tmpSenha, entrada;
-    User usuario;
+    //User usuario;
 
-    while((tmpNome.length() > 1) == false){
+	do
+	{
+		std::cout << LINE << "Você deseja: \n [1] Fazer login \n [2] Cadastrar-se \n >> " << LINE;
+		std::cin >> cmd;
+		if (std::cin.fail()) // Se o voce inserir um valor errado no cin essa funcao ira "limpar" ele e retornara o valor 0
+		{
+			std::cin.clear();//Por isso nunca atribua no switch case uma funcao chamada pelo valor 0
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		switch(cmd)
+		{
+			case 1:
+			{
+				login fazer_login;
+				fazer_login.verificar_usuario();
+				fazer_login.registrar_acesso();
+				break;
+			}
+			case 2:
+			{
+				break;
+			}
+			default:
+			{
+                std::cout << "Numero invalido, tente novamente:\n";
+                break;
+			}
+		}
+	} while (cmd < 1 || cmd > 2);
+	
+
+    /*while((tmpNome.length() > 1) == false){
     	std::cout << LINE << "Você deseja: \n [0] Fazer login \n [1] Cadastrar-se \n >> " << LINE;
     	std::cin >> cmd;
 
 	    if(cmd == 1){
+			login fazer_login;
+			fazer_login.verificar_usuario();
+			fazer_login.registrar_acesso();
+			/*
         	std::cout << "Digite o seu nome: ";
 	        std::cin >> tmpNome;
         	std::cout << "Digite o seu email: ";
@@ -35,15 +72,13 @@ int main(){
 			tmpNome = "";
 			tmpEmail = "";
 		}else{
-        		usuario = User(tmpNome, tmpEmail, tmpSenha);
+        		//usuario = User(tmpNome, tmpEmail, tmpSenha);
 		}
 		tmpSenha = "";
-    	}
-    }
-    
+    	}*/
 
     //limpando a memoria
-    delete cardapio;
+    //delete cardapio;
 
     return SUCESSO;
 }
